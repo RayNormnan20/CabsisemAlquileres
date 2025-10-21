643e103deec8383b6717df8a8c840d5924655026
RayNormnan20 <josepintadoyamo@gmail.com>
2025-10-21 01:36:47 -0500

    fix abonos opciones

--- resources/views/filament/resources/abonos-resource/header.blade.php
+++ resources/views/filament/resources/abonos-resource/header.blade.php
@@ -150,8 +150,8 @@
             <!-- Botón Crear Abono -->
             <div class="relative inline-block text-left" x-data="{ open: false }">
                 <button @click="open = !open" @click.away="open = false"
-                    class="inline-flex items-center px-4 py-2 bg-primary-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 transition ease-in-out duration-150 {{ !($clienteId ?? null) ? 'opacity-50 cursor-not-allowed' : '' }}"
-                :class="{ 'bg-primary-700': open }" :disabled="!{{ $clienteId ?? 'null' }}">
+                    class="inline-flex items-center px-4 py-2 bg-primary-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                :class="{ 'bg-primary-700': open }">
                     Abonos
                     <svg class="-mr-1 ml-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                         fill="currentColor">
